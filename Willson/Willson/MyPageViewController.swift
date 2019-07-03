bc65221ad55a0a7b525c38cdaf7868cf50212a23
jisu park <qkrwltn8976@naver.com>
2019-07-04 00:20:38 +0900

    completed askermypage

---
+++ Willson/Willson/MyPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyPageViewController.swift
+//  Willson
+//
+//  Created by 박지수 on 03/07/2019.
+//  Copyright © 2019 JaehuiKim. All rights reserved.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
