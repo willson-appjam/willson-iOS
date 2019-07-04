007d500a28d9cbb9a6e265fd5016d1ed7f400191
jaehui327 <jaehui327@naver.com>
2019-07-04 16:32:17 +0900

    Add Storyboards, view controllers

---
+++ Willson/Willson/view controllers/helper/chat/HelperChatListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HelperChatListViewController.swift
+//  Willson
+//
+//  Created by JHKim on 04/07/2019.
+//  Copyright © 2019 JaehuiKim. All rights reserved.
+//
+
+import UIKit
+
+class HelperChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
