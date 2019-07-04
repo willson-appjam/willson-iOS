9cb7e7571f785124f4ea6ae642c31d83cc7e051e
jaehui327 <jaehui327@naver.com>
2019-07-05 05:27:06 +0900

    Update

--- Willson/Willson/view controllers/asker/chat/ReviewStarViewController.swift
+++ Willson/Willson/view controllers/asker/chat/ReviewStarViewController.swift
@@ -18,24 +18,49 @@ class ReviewStarViewController: UIViewController {
     
     // MARK: - IBAction
     @IBAction func tappedCheckButton(_ sender: Any) {
+        
     }
     
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // add TapGestrue at StackView
+        let starGesture = UILongPressGestureRecognizer(target: self, action: #selector(draggingStars(_:)))
+        starStackView.addGestureRecognizer(starGesture)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    @objc private func draggingStars(_ gesture: UILongPressGestureRecognizer) {
+        self.view.endEditing(true)
+        let point = gesture.location(in: starStackView)
+        
+        switch gesture.state {
+        case .began, .changed: changingStars(point.x)
+        default: break;
+        }
     }
-    */
+       
+    private func changingStars(_ point: CGFloat) {
+        var ratingNumber = 0.0
+        
+        let viewWidth = starStackView.frame.size.width / 10
+        let divideRating = Int(point / viewWidth / 2)
+        
+        var starImage =  UIImage(named: "chatBtnReviewStarBlank")
+        for (index, subview) in starStackView.arrangedSubviews.enumerated() {
+            if let imageSubview = subview as? UIImageView {
+                if (index < divideRating) {
+                    starImage =  UIImage(named: "chatBtnReviewStarYellow")
+                    ratingNumber += 1.0
+                } else {
+                    starImage = UIImage(named: "chatBtnReviewStarBlank")
+                }
+                imageSubview.image = starImage
+            }
+        }
+        
+        selectStarLabel.text = "\(Int(ratingNumber))"
+    }
 
 }
