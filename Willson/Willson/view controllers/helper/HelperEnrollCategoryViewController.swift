70af9c56b780126fed5ed4c067b333905b572c9b
jisu park <qkrwltn8976@naver.com>
2019-07-06 02:42:45 +0900

    updated Helper Enrollment views

--- Willson/Willson/view controllers/helper/HelperEnrollCategoryViewController.swift
+++ Willson/Willson/view controllers/helper/HelperEnrollCategoryViewController.swift
@@ -10,21 +10,70 @@ import UIKit
 
 class HelperEnrollCategoryViewController: UIViewController {
 
+    let categoryCollectionCellIdentifier: String = "ConcernCollectionViewCell"
+    
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        categoryCollectionView.delegate = self
+        categoryCollectionView.dataSource = self
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+extension HelperEnrollCategoryViewController: UICollectionViewDelegate {
+    
+}
+
+extension HelperEnrollCategoryViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell: ConcernCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: categoryCollectionCellIdentifier, for: indexPath) as? ConcernCollectionViewCell else { return UICollectionViewCell() }
+        
+        switch indexPath.row {
+        case 0:
+            cell.concernLabel.isHidden = false
+            cell.concernTextField.isHidden = true
+            cell.concernLabel.text = "연애"
+        case 1:
+            cell.concernLabel.isHidden = false
+            cell.concernTextField.isHidden = true
+            cell.concernLabel.text = "진로"
+        case 2:
+            cell.concernLabel.isHidden = false
+            cell.concernTextField.isHidden = true
+            cell.concernLabel.text = "심리"
+        case 3:
+            cell.concernLabel.isHidden = false
+            cell.concernTextField.isHidden = true
+            cell.concernLabel.text = "인간관계"
+        case 4:
+            cell.concernLabel.isHidden = false
+            cell.concernTextField.isHidden = true
+            cell.concernLabel.text = "일상"
+        case 5:
+            cell.concernLabel.isHidden = false
+            cell.concernTextField.isHidden = true
+            cell.concernLabel.text = "기타"
+        default:
+            return cell
+        }
+        
+        return cell
+    }
+    
+    
+}
+
+extension HelperEnrollCategoryViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 153.0, height: 50.0)
+    }
+}
+
+
