9c87ab63c02e408e44bc6763df1022c18ac82771
jaehui327 <jaehui327@naver.com>
2019-07-06 08:36:05 +0900

    Update

---
+++ Willson/Willson/view controllers/helper/request/views/HelperRequest\CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  HelperRequest\CollectionViewCell.swift
+//  Willson
+//
+//  Created by JHKim on 06/07/2019.
+//  Copyright © 2019 JaehuiKim. All rights reserved.
+//
+
+import UIKit
+
+class HelperRequest_CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
