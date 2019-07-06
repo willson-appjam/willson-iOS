14fdff6d8773542ca1e04fddb99d50a2b6fd018c
jisu park <qkrwltn8976@naver.com>
2019-07-06 21:22:16 +0900

    added chatroom headercell

---
+++ Willson/Willson/view controllers/helper/chat/HelperChatRoomViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HelperChatRoomViewController.swift
+//  Willson
+//
+//  Created by 박지수 on 06/07/2019.
+//  Copyright © 2019 JaehuiKim. All rights reserved.
+//
+
+import UIKit
+
+class HelperChatRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationItem.title = "리트리버" + " 님"
+    }
+
+}
