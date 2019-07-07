8d67e83b52dd162c636410411adf793e999a6a39
jaehui327 <jaehui327@naver.com>
2019-07-08 06:16:51 +0900

    Update AskerSignUp autolayout

--- Willson/Willson/supporting files/fonts/AskerSignUpViewController1.swift
+++ Willson/Willson/supporting files/fonts/AskerSignUpViewController1.swift
@@ -11,26 +11,41 @@ import DLRadioButton
 
 class AskerSignUpViewController1: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
+    // MARK: - IBOutlet
     @IBOutlet weak var pickerTextField: UITextField!
-   
+    
     @IBOutlet weak var nextBtn: UIButton!
     
+    // MARK: - properties
     let salutations = ["20세", "21세","22세","23세","24세","25세","26세","27세","28세","29세","30세","31세","32세","33세","34세","35세","36세"]
-
+    
+    // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let pickerView = UIPickerView()
         pickerView.delegate = self
         
-    
+        
         pickerTextField.inputView = pickerView
+        
+        //빈화면 탭했을 때 제스처 추가
+        let tap = UITapGestureRecognizer(target: self, action: #selector(viewDidTapped(_:)))
+        view.addGestureRecognizer(tap)
     }
     
+    // MARK: - IBAction
     @IBAction func nextBtnAction(_ sender: Any) {
-        let dvc = self.storyboard?.instantiateViewController(withIdentifier: "AskerSignUpViewController2") as! AskerSignUpViewController2
+        guard let dvc: AskerSignUpViewController2 = self.storyboard?.instantiateViewController(withIdentifier: "AskerSignUpViewController2") as? AskerSignUpViewController2 else { return }
         
         self.present(dvc, animated: true)
+    }
+    
+    // MARK: - Methods
+    
+    //빈 화면 탭했을 때 키보드 내리기
+    @objc func viewDidTapped(_ sender: UITapGestureRecognizer) {
+           view.endEditing(true)
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -49,15 +64,4 @@ class AskerSignUpViewController1: UIViewController, UIPickerViewDataSource, UIPi
         return salutations[row]
     }
     
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
