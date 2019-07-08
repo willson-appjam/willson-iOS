61139764fd01abcb49f383d0578f6e5e926c2111
jaehui327 <jaehui327@naver.com>
2019-07-09 01:44:09 +0900

    Oh~yeah

--- Willson/Willson/view controllers/asker/request/CollectionReusableView.swift
+++ Willson/Willson/view controllers/asker/request/CollectionReusableView.swift
@@ -2,9 +2,11 @@
 //  CollectionReusableView.swift
 //  Willson
 //
-//  Created by 박지수 on 03/07/2019.
+//  Created by JHKim on 09/07/2019.
 //  Copyright © 2019 JaehuiKim. All rights reserved.
 //
+
+import Foundation
 
 import UIKit
 
