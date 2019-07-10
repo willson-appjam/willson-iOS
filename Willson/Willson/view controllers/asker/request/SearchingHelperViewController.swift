95716384933172572c5fb34da55b6efd6e8c637b
jaehui327 <jaehui327@naver.com>
2019-07-10 17:31:46 +0900

    Update searchingHelper

---
+++ Willson/Willson/view controllers/asker/request/SearchingHelperViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SearchingHelperViewController.swift
+//  Willson
+//
+//  Created by JHKim on 10/07/2019.
+//  Copyright © 2019 JaehuiKim. All rights reserved.
+//
+
+import UIKit
+
+class SearchingHelperViewController: UIViewController {
+
+    // MARK: - properties
+    
+    // MARK: - IBOutlet
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    // MARK: - life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
